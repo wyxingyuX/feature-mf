f3fbd68a3707a1385a80d009d828d00722ebe5d6
wyxingyuX <wyxingyu@gmail.com>
2016-10-05 22:41:53 +0800

    commit

--- MCL.py
+++ MCL.py
@@ -1,3 +1,23 @@
 # -*- coding:utf-8 -*-
 # Paper: Multi-View Concept Learning for Data Representation
 
+import numpy as np
+
+
+def mcl(views_X, a, b, r, nn, k):
+    # Randomly initialize (Uik)v>=0, 1>=Vkj>=o, any j,k,v
+    views_U = []
+    V = np.random.uniform(0, 1, size=nn * k).reshape(nn, k)
+    for i in range(len(views_X)):
+        m,n=views_X[i].shape
+        U=np.random.uniform(0,1,size=m*k).reshape(m,k)
+        views_U.append(U)
+
+    while True:
+        pass
+
+    return views_U, V
+
+
+if __name__ == "__main__":
+    print "hello"
